db266c9ac2e43bf2d43c4cac8d308a2a8d80c7c1
황인성 <me@insd.dev>
2026-06-23 14:42:28 +0900

    refactor: 더 깔끔한 사이트맵 코드

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,22 +1,21 @@
 import type { MetadataRoute } from 'next';
-import { posts } from '@/lib/content/posts';
+import posts from '@/lib/content/posts';
 import config from '@/lib/config';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const url = config.metadata.url;
+  const lastModified = new Date();
+  const pages = ['', '/posts', '/projects'];
   const allPosts = await posts.list();
 
   return [
-    {
-      url: config.metadata.url,
-      lastModified: new Date(),
-    },
-    {
-      url: `${config.metadata.url}/posts`,
-      lastModified: new Date(),
-    },
-    ...allPosts.map((post) => ({
-      url: `${config.metadata.url}/posts/${post.slug}`,
-      lastModified: new Date(post.date),
+    ...pages.map((page) => ({
+      url: url + page,
+      lastModified,
+    })),
+    ...allPosts.map(({ slug, date }) => ({
+      url: `${url}/posts/${slug}`,
+      lastModified: date,
     })),
   ];
 }
--- src/lib/content/posts/index.ts
+++ src/lib/content/posts/index.ts
@@ -4,7 +4,9 @@ import { importSlugs, importPost } from '@/lib/content/posts/importer';
 const posts = {
   list: cache(async () => {
     const slugs = await importSlugs();
-    const items = await Promise.all(slugs.map((slug) => importPost(slug)));
+    const items = await Promise.all(
+      slugs.map(async (slug) => ({ slug, ...(await importPost(slug)) })),
+    );
 
     return items.sort((a, b) => b.date.getTime() - a.date.getTime());
   }),
@@ -12,5 +14,4 @@ const posts = {
   get: cache(async (slug: string) => await importPost(slug)),
 };
 
-export type Post = Awaited<ReturnType<typeof posts.get>>;
 export default posts;
