12d06b687ac04d4074c8f0bdcd8e7c246d229090
황인성 <me@insd.dev>
2026-06-27 21:09:59 +0900

    refactor: 게임 카드를 원격 데이터 로드에 최적화

---
+++ src/app/_views/games/sdvx.tsx
@@ -0,0 +1,22 @@
+import GameCard from '@/components/cards/game';
+
+export default async function SdvxCard() {
+  return (
+    <GameCard
+      icon={icon}
+      title="Sound Voltex"
+      rating={21.309}
+      grade={{ name: 'IMPERIAL II', gap: 1 }}
+      colors={['#BAE969', '#0E1808', '#D6D6D6']}
+    />
+  );
+}
+
+const icon = (
+  <svg width="17" height="10" viewBox="0 0 17 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M4.86911 1.36126L7.06806 0H16.3351L14.0838 1.72775H9.21466L7.12042 3.50785L6.96335 4.60733L10.2094 2.87958L9.73822 4.71204L13.0367 2.87958L11.5707 8.37696L9.10995 10H0L2.04189 8.27225H7.06806L9.05759 6.43979L9.68587 5.02618L6.17801 7.12042L6.49215 5.07853L3.29843 6.85864L4.86911 1.36126Z"
+      fill="currentColor"
+    />
+  </svg>
+);
