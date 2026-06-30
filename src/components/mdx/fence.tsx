0468cd31a8b85e290eafa739ace5ff6c78363b16
황인성 <me@insd.dev>
2026-06-30 15:43:32 +0900

    feat: 코드블럭 아이콘 추가

--- src/components/mdx/fence.tsx
+++ src/components/mdx/fence.tsx
@@ -4,9 +4,12 @@ import { ComponentProps, useRef } from 'react';
 import { cn } from '@/lib/utils/cn';
 import { useFeedback } from '@/lib/hooks/feedback';
 import { CheckIcon, CopyIcon } from 'lucide-react';
+import { resolveFileIcon } from '@insd47/ide-file-icons';
+import Image from 'next/image';
 
 export function Pre({ className, title, ...props }: ComponentProps<'pre'>) {
   const ref = useRef<HTMLPreElement>(null);
+  const icon = title ? resolveFileIcon(title, 'dark') : null;
 
   const [onClick, copied] = useFeedback(async () => {
     if (ref.current) {
@@ -17,14 +20,21 @@ export function Pre({ className, title, ...props }: ComponentProps<'pre'>) {
   return (
     <figure className="relative p-0! bg-foreground/1 text-[13px] border-y">
       {title && (
-        <figcaption className="bg-foreground/2 font-display h-9 flex items-center px-5 border-b">
-          <p>{title}</p>
+        <figcaption className="bg-foreground/2 h-9 gap-1 flex items-center px-5 border-b">
+          <Image
+            unoptimized
+            src={`https://intellij-icons.jetbrains.design/icons/${icon}`}
+            alt="file icon"
+            width={16}
+            height={16}
+          />
+          {title}
         </figcaption>
       )}
 
       <button
         className={cn(
-          'absolute flex flex-center top-1.75 right-2 size-5',
+          'absolute flex flex-center top-1.75 right-2.25 size-5',
           ' *:size-3.5 *:transition-[color,opacity] *:text-foreground/60 hover:*:text-foreground',
         )}
         onClick={onClick}
